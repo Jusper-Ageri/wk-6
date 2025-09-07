cc50af9dd1235e457f74ac90312421d5a38cd88b
Jusper-Ageri <ombokajusper@gmail.com>
2025-09-07 13:30:53 +0300

    Update answers.sql

--- answers.sql
+++ answers.sql
@@ -1,4 +1,5 @@
--- Question 1:
+-- Question 1: INNER JOIN combining employees table with offices table using officeCode column
+
 USE salesdb;
 
 SELECT 
@@ -9,3 +10,33 @@ SELECT
 FROM employees
 INNER JOIN offices
     ON employees.officeCode = offices.officeCode;
+
+-- Question 2: LEFT JOIN combining products table with productline table using productLine column
+
+USE salesdb;
+
+SELECT 
+    products.productName,
+    products.productVendor,
+    productlines.productLine
+FROM products
+LEFT JOIN productlines
+    ON products.productLine = productlines.productLine;
+
+-- Question 3: RIGHT JOIN combining customers table with the orders table using the customer number column
+
+USE salesdb;
+
+SELECT 
+    orders.orderDate,
+    orders.shippedDate,
+    orders.status,
+    orders.customerNumber
+FROM customers
+RIGHT JOIN orders
+    ON customers.customerNumber = orders.customerNumber
+LIMIT 10;
+
+
+
+
